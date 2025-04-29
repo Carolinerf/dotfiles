a671a28950a2d95214d8a309f0962f82969ac0d7
Carolinerf <holmes00h@gmail.com>
2025-04-29 21:28:50 +0800

    add rust for neovim

--- nvim/lua/configs/rust-tools.lua
+++ nvim/lua/configs/rust-tools.lua
@@ -1,15 +1,6 @@
 local rt = require("rust-tools")
 rt.setup({
     server = {
-        on_attach = function(_, bufnr)
-            -- 示例按键
-            vim.keymap.set(
-                "n",
-                "<Leader>hh",
-                rt.hover_actions.hover_actions,
-                { buffer = bufnr }
-            )
-        end,
         settings = { ["rust-analyzer"] = { cargo = { allFeatures = true } } },
     },
     tools = { inlay_hints = { auto = true } },
